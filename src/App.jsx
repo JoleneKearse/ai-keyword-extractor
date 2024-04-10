c8c17ed4c688cd56477346daedd433d18d929fb5
JoleneKearse <pharaohnutz@gmail.com>
2024-04-10 13:48:52 -0400

    feat: add TextInput with toast for empty submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
+import TextInput from "./components/TextInput";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <Box bgGradient="linear(to-r, cyan.500, purple.600)" color="purple.50" height="100vh" paddingTop={130}>
       <Container maxW="3xl" centerContent>
         <Header />
+        <TextInput />
         <Footer />
       </Container>
     </Box>
