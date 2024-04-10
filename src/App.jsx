12602a2cc6b049677ad469e6934384ac91a253a6
JoleneKearse <pharaohnutz@gmail.com>
2024-04-10 12:07:49 -0400

    chore: add initial app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import { Container, Box } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <h1>My App</h1>
-    </>
+    <Box bgGradient="linear(to-r, cyan.500, purple.600)" color="purple.50" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        <h1>My App</h1>
+      </Container>
+    </Box>
   )
 }
 
