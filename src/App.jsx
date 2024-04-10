fdc35f3ae40be824252a2c48170713e892e9364f
JoleneKearse <pharaohnutz@gmail.com>
2024-04-10 12:45:57 -0400

    chore: add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <Box bgGradient="linear(to-r, cyan.500, purple.600)" color="purple.50" height="100vh" paddingTop={130}>
       <Container maxW="3xl" centerContent>
         <Header />
-        {/* <h1>Hello there!</h1> */}
+        <Footer />
       </Container>
     </Box>
   )
